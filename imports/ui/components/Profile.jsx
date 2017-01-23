d9b756852b3e231fd29377393d10505beae180ba
Uğur Kafalı <ugurkafali@ogr.cbu.edu.tr>
2017-01-23 16:56:13 +0300

    0.5.4
    Boards from trello now properly is displayed in profile

--- imports/ui/components/Profile.jsx
+++ imports/ui/components/Profile.jsx
@@ -35,19 +35,22 @@ export default class Profile extends Component {
 
     this.connectToTrello = this.connectToTrello.bind(this);
     this.onAuthorize = this.onAuthorize.bind(this);
+    this.onAuthorize2 = this.onAuthorize2.bind(this);
     this.exitFromTrello = this.exitFromTrello.bind(this);
     this.updateLogin = this.updateLogin.bind(this);
     this.handleDisabled = this.handleDisabled.bind(this);
     this.handleDisabled2 = this.handleDisabled2.bind(this);
 
-
+    Trello.authorize({
+      interactive:false,
+      success: this.onAuthorize2
+    });
   }
 
   connectToTrello(){
     Trello.authorize({
       name: "PROJECT",
       type: "popup",
-      persist: false,
       success: this.onAuthorize
     })
   }
@@ -60,14 +63,66 @@ export default class Profile extends Component {
 
   exitFromTrello() {
     Trello.deauthorize();
+    $(".output").empty();
     this.handleDisabled2();
     this.handleDisabled();
   }
 
-  onAuthorize() {
+  onAuthorize() {  // Boardları yazdıracağımız yer
     this.handleDisabled();
     this.handleDisabled2();
-    console.log('OK');
+
+    $(".output").empty();
+
+    Trello.members.get("me", function(member){
+        //$("#fullName").text(member.fullName);
+
+        var $cards = $("<div>")
+            .text("Loading Cards...")
+            .appendTo(".output");
+
+        // Output a list of all of the cards that the member
+        // is assigned to
+        //Trello.get("lists/5853d4b1d571299a7aeeb3d5/cards", function(cards) { Belirli bir listedeki cardları alıyo
+        //Trello.get("boards/581379388d608e841d188d97/cards", function(cards) {  Bütün cardları alıyo
+        Trello.get("/member/me/boards", function(cards) {
+            $cards.empty();
+            $.each(cards, function(ix, card) {
+                $("<a>")
+                .attr({href: card.url, target: "trello"})
+                .addClass("card")
+                .text(card.name)
+                .appendTo($cards);
+            });//each
+        });//Trello.get ++ boards
+    });//Trello.get ++ name
+  }
+
+  onAuthorize2() {
+    $(".output").empty();
+
+    Trello.members.get("me", function(member){
+        //$("#fullName").text(member.fullName);
+
+        var $cards = $("<div>")
+            .text("Loading Cards...")
+            .appendTo(".output");
+
+        // Output a list of all of the cards that the member
+        // is assigned to
+        //Trello.get("lists/5853d4b1d571299a7aeeb3d5/cards", function(cards) { Belirli bir listedeki cardları alıyo
+        //Trello.get("boards/581379388d608e841d188d97/cards", function(cards) {  Bütün cardları alıyo
+        Trello.get("/member/me/boards", function(cards) {
+            $cards.empty();
+            $.each(cards, function(ix, card) {
+                $("<a>")
+                .attr({href: card.url, target: "trello"})
+                .addClass("card")
+                .text(card.name)
+                .appendTo($cards);
+            });//each
+        });//Trello.get ++ boards
+    });//Trello.get ++ name
   }
 
   handleDisabled() {
@@ -96,6 +151,7 @@ export default class Profile extends Component {
           <CardActions>
             <FlatButton disabled={this.state.disabled} className="connect" label="Connect to Trello" onTouchTap={this.connectToTrello}/>
             <FlatButton disabled={this.state.disabled2} className="exit" label="Disconnect from Trello" onTouchTap={this.exitFromTrello}/>
+            <div className="output"></div>
           </CardActions>
         </Card>
       </MuiThemeProvider>
