531b876f19144d241db76833a2a81cd403f0f19f
Abdullah Öğük <abdullahoguk@gmail.com>
2017-03-12 14:37:46 +0300

    1.1.4.1
    hotfix

---
+++ imports/ui/components/Clock.jsx
@@ -0,0 +1,68 @@
+import React, { Component, constructor, State } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CircularProgress from 'material-ui/CircularProgress';
+import ReactCSSTransition from 'react-addons-css-transition-group';
+import Rsvg from 'react-inlinesvg';
+
+export default class Clock extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getMinutes = this.getMinutes.bind(this);
+    this.getSeconds = this.getSeconds.bind(this);
+    this.drawMinutes = this.drawMinutes.bind(this);
+    this.drawSeconds = this.drawSeconds.bind(this);
+  }
+
+  getMinutes(){
+    var minutes = 25 - this.props.elapsedTime / 60;
+    return parseInt(minutes);
+  }
+
+  getSeconds(){
+    var seconds = (1500 - this.props.elapsedTime) % 60;
+    return parseInt(seconds);
+  }
+
+  drawMinutes(){
+    var temp = this.getMinutes();
+    if (temp < 10) {
+      return '0' + temp.toString();
+    } else {
+      return temp;
+    }
+  }
+
+  drawSeconds(){
+    var temp = this.getSeconds();
+    if (temp < 10) {
+      return '0' + temp.toString();
+    } else {
+      return temp;
+    }
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div className="clock logo">
+          <div className="circular">
+            <CircularProgress
+              color="white"
+              mode="determinate"
+              value={100 - this.props.elapsedAngle}
+              size={288}
+              thickness={12}
+            />
+          </div>
+          <div className = "clockLogo">
+              <img src="dakik_logo.svg" alt=""/>
+            </div>
+          <div className="clockText">
+            {this.drawMinutes()}:{this.drawSeconds()}
+          </div>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
